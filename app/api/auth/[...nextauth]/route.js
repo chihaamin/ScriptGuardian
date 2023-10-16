44153f03e01ee784d3da3f07e405cc68517c5f69
XEKEX <118768524+chihaamin@users.noreply.github.com>
2023-10-16 20:27:07 +0100

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -128,7 +128,7 @@ const handler = NextAuth({
     },
     providers: [
         CredentialsProvider({
-            name: 'Credentials',
+            name: 'credentials',
             credentials: {
                 auth: {},
                 pwd: {},
@@ -136,9 +136,9 @@ const handler = NextAuth({
             authorize: async (credentials) => {
                 const user = await customLogin(credentials);
                 if (user) {
-                    return Promise.resolve(user);
+                    return user;
                 } else {
-                    return Promise.resolve(null);
+                    return null;
                 }
             },
         }),

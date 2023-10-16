4cfc140b13706f43b5ab63ab1afba301c8640241
XEKEX <amin.chiha@gmail.com>
2023-10-17 00:56:30 +0100

    ui

--- components/userprofile.js
+++ components/userprofile.js
@@ -3,19 +3,26 @@ import { DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar, User } f
 import { useSession, signOut } from 'next-auth/react';
 import { useRouter } from "next/navigation";
 const UserProfile = () => {
-    const { data: session,status } = useSession();
+    const { data: session, status } = useSession();
     const route = useRouter()
     if (status == 'authenticated') () => route.push('/');
     return (
         <Dropdown placement="bottom-end">
-            <DropdownTrigger>
-                <User
+            <DropdownTrigger className="flex">
+                <Avatar
                     as="button"
                     className="transition-transform"
                     description={ session.user.title }
                     name={ session.user.name }
-                    avatarProps={ { src: session.user.img } }
+                    src={ session.user.img }
+                    size="lg"
                 />
+                <div className="h-full block">
+                    <p>
+                        { session.user.name }
+                    </p>
+                    <p>{ session.user.title }</p>
+                </div>
             </DropdownTrigger>
             <DropdownMenu aria-label="Profile Actions" variant="flat" disabledKeys={ [ "settings", "analytics", "system", "membership" ] }>
                 <DropdownItem key="membership" >Membership</DropdownItem>

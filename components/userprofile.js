ff4b7cab25c55c1992a51e000c6e3a0a9ef0edb4
XEKEX <amin.chiha@gmail.com>
2023-10-17 01:31:30 +0100

    ui

--- components/userprofile.js
+++ components/userprofile.js
@@ -9,14 +9,19 @@ const UserProfile = () => {
     return (
         <Dropdown placement="bottom-end">
             <DropdownTrigger className="flex">
-                <Avatar
-                    as="button"
-                    className="transition-transform"
-                    name={ session.user.name }
-                    src={ session.user.img }
-                    size="md"
-                    
-                />
+                <div className='flex justify-center items-center gap-1 w-auto cursor-pointer'> 
+                <button class="relative justify-center items-center box-border overflow-hidden align-middle outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 w-10 h-10 text-tiny bg-default text-default-foreground rounded-full z-10 aria-expanded:scale-[0.97] aria-expanded:opacity-70 subpixel-antialiased transition-transform flex" type="button">
+                    <img src={ session.user.img } class="flex object-cover w-full h-full transition-opacity !duration-500 opacity-0 data-[loaded=true]:opacity-100" alt={ session.user.name } />
+                    </button>
+                    <div className="flex-row ">
+                        <p className="font-bold text-sm text-violet-800">
+                            { session.user.name }
+                        </p>
+                        <p className="text-tiny text-stone-600">
+                            { session.user.title }
+                        </p>
+                    </div>
+                    </div>
             </DropdownTrigger>
             <DropdownMenu aria-label="Profile Actions" color='secondary' variant="shadow" disabledKeys={ [ "settings", "analytics", "system", "membership" ] }>
                 <DropdownItem key="membership" >Membership</DropdownItem>
